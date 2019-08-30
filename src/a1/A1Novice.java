34c455bbc654b385649f57e01b5c05932a40c088
Shaswat Joshi <joshishaswat@gmail.com>
2019-08-30 11:35:58 -0400

    added text file

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,16 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
+		
+		int total, loop;
+		
 		// Your code follows here.
+		loop = scan.nextInt();
+//		loop = System.out.println(scan.next());
+		
+		for (int i = 0; i < loop; i++) {
+			System.out.println("here");
+		}
 		
 	}
 }
